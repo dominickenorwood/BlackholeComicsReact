56200d0b121da4efc88da7678f38d44a6b32ba97
Dominick Norwood <dominicklelander@gmail.com>
2018-05-13 22:05:48 -0400

    Redux store directory created, authentication actions created

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import Aux from './hoc/Auxillary/Auxillary';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Aux>
+        <h1>Blackhole Comics</h1>
+      </Aux>
     );
   }
 }
---
+++ src/store/actions/auth.js
@@ -0,0 +1,106 @@
+// axios endpoint instances
+import authNewUser from '../../api/authNewUser';
+import authExistingUser from '../../api/authExistingUser';
+
+// constants clusters
+import * as keys from '../../api/keys';
+import * as actionTypes from './actionTypes';
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    }
+}
+
+export const authSuccess = (token, userId) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        token,
+        userId
+    }
+}
+
+export const authFail = error => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error
+    }
+}
+
+export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
+
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+}
+
+export const checkAuthTimeout = expirationTime => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime);
+    }
+}
+
+export const auth = (email, password, isRegister = false) => {
+    return dispatch => {
+        dispatch(authStart());
+
+        const authData = {
+            returnSecureToken: true,
+            email,
+            password
+        };
+
+        let instance = authNewUser;
+        if(!isRegister){
+            instance = authExistingUser;
+        }
+
+        instance.post(keys.AUTH_KEY, authData)
+            .then(response => {
+                console.log(response);
+                const tokenExpiration = response.data.expiresIn;
+
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('tokenExpiration', tokenExpiration);
+                localStorage.setItem('userId', response.data.localId);
+
+                dispatch(authSuccess(response.data.token, response.data.localId));
+                dispatch(checkAuthTimeout(tokenExpiration));
+            })
+            .catch(err => {
+                console.log('[CATCH ERROR]', err.response.data.error);
+                dispatch(authFail(err.response.data.error));
+            });
+    }
+}
+
+export const setAuthRedirectPath = path => {
+    return {
+        type: actionTypes.SET_AUTH_REDIRECT_PATH,
+        path
+    }
+}
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+
+        if(!token){
+            dispatch(logout());
+        } else {
+            const tokenExpiration = new Date(localStorage.getItem('tokenExpiration'));
+            if(tokenExpiration <= new Date()){
+                dispatch(logout());
+            } else {
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout(tokenExpiration));
+            }
+        }
+    }
+}
