fe53ac63b688f5a522faf37016188aa924f675a3
dominickenorwood <dominickenorwood@gamil.com>
2018-09-07 09:50:39 -0400

    Testing uploading an asset

--- src/containers/Comic/Comic.js
+++ src/containers/Comic/Comic.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import classes from './Comic.css';
 
 class Comic extends Component {
     state = {
         valid: true,
         errors: [],
-        success: []
+        success: [],
+        selectedFile: null
+    }
+
+    fileChangeHandler = event => {
+        this.setState({ selectedFile: event.target.files[0] })
+    }
+
+    uploadHandler = event => {
+        console.log(this.state.selectedFile);
+        axios.post('https://blackhole-comics.appspot.com', this.state.selectedFile)
+            .then(response => {
+                console.log('Uploaded Image')
+                console.log(response);
+            })
+            .catch(error => console.log('[Error]', error.response))
     }
 
     render() {
 
         return (
-            <div>Add a comic</div>
+            <div className={ classes.Layout }>
+                <input type="file" onChange={ this.fileChangeHandler } />
+                <button onClick={ this.uploadHandler }>Upload!</button>
+            </div>
         )
     }
 }
